6f54e9f6238b3dd786d6b97f31bd4386591ad2e4
Sheng Wei Zhu <shengwei.zhu@stonybrook.edu>
2021-07-03 21:37:54 -0400

    centered the game window

---
+++ src/game.js
@@ -0,0 +1,108 @@
+//Author: Sheng Wei, Joshua
+
+SCREEN_WIDTH = 1280;
+SCREEN_HEIGHT = 720;
+FPS = 60;
+
+let data = [];
+
+//The code gets called here.
+function initialize()
+{
+    console.log("Initializing.");
+    //sets the size of the canvas.
+    let canvas = document.getElementById("gamewindow");
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    clearGameWindow();
+    addPlayer();
+    start();
+}
+
+
+//maingame loop function
+async function start()
+{
+    let previousTime = Date.now(); //gets the current time in milliseconds
+    //scuffed game loop.
+    while(true)
+    {
+        tick();
+        render();
+        //calculate the time needed to sleep based on the FPS.
+        let singleFrameTime = (1.0/FPS) * 1000; //calculate the maximum number of ms perframe.
+        let currentTime = Date.now();
+        let timePassed = currentTime - previousTime;
+        let sleepTime = singleFrameTime - timePassed;
+        if(sleepTime > 0)
+            await sleep(sleepTime);
+        previousTime = Date.now();
+    }
+}
+
+//all physics calculations will go in here.
+function tick()
+{
+    data.forEach(node => {
+        if(node.type == "player")
+        {
+            if(node.x > SCREEN_WIDTH || node.x < 0)
+                node.velocityX = -node.velocityX;
+            if(node.y > SCREEN_HEIGHT || node.y < 0)
+                node.velocityY = -node.velocityY;
+            node.x += node.velocityX;
+            node.y += node.velocityY;
+        }
+    });
+}
+
+//all gamewindow drawing will go in here.
+function render()
+{
+    let canvas = document.getElementById("gamewindow");
+    let ctx = canvas.getContext("2d");
+    //first draw the background.
+    clearGameWindow();
+
+    //draw the game nodes, like players and enemies.
+    data.forEach(node => {
+        if(node.type == "player")
+            ctx.fillStyle = "green";
+        ctx.fillRect(node.x, node.y, node.width, node.height);
+    });
+}
+
+
+//------------------------  HELPER FUNCTIONS ------------------------------
+/** This function draws the background of the game window. */
+function clearGameWindow()
+{
+    let canvas = document.getElementById("gamewindow");
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+/**
+ * @param ms - The number of milliseconds to sleep the program.
+ */
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Creates a new player by adding an entry to data.
+ */
+function addPlayer()
+{
+    data.push({
+        type: "player",
+        x: 500,
+        y: 500,
+        width: 10,
+        height: 10,
+        velocityX: 3,
+        velocityY: 5
+    });
+}
